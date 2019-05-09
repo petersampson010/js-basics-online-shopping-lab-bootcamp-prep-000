1368a52947e833db2c6f488676f30c51ac859bd5
Peter Sampson <petersampson010@gmail.com>
2019-05-09 18:50:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,18 +26,11 @@ function viewCart() {
         itemList.push(`${item} at $${cart[i][item]}`)
       }
     }
+    return `In your cart, you have ${items.join()
   }
 }
-}
 
-function viewCart() {
-  if (cart.length === 0) {
-    console.log("Your shopping cart is empty.")
-  } else {
-    var items = [];
-    for (var i = 0; i < cart.length; i++) {
-      for (var item in cart[i]) {
-        items.push(item + " at $" + cart[i][item])
+`
       }
     }
     console.log("In your cart, you have " + items.join(", ") + ".");
