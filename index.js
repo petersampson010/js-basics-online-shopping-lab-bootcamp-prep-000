265930ae7dd0a419b446d2d8b5b95f05d60309ef
Peter Sampson <petersampson010@gmail.com>
2019-05-09 18:37:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,19 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var random100 = Math.floor(Math.random()*101)
+  var firstObject = {itemName: item, itemPrice: random100}
+  cart.push(firstObject)
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
+  var Obj = new Object({cart})
+  if (cart.length===0) {
+    return `Your shopping cart is empty.`
+  } else if (cart.length===1) {
+      return `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`
+  }
 }
 
 function removeFromCart(item) {
